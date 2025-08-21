628b9570f6bfce1a09c0516ec699bbdec93b6822
suryaandh <gdswiyasa@gmail.com>
2025-08-22 05:36:45 +0700

    Initial Commit

---
+++ PixelPlay/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PixelPlay
+//
+//  Created by Zhuanz1密码0000 on 22/08/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PixelPlay/PixelPlayApp.swift
@@ -0,0 +1,17 @@
+//
+//  PixelPlayApp.swift
+//  PixelPlay
+//
+//  Created by Zhuanz1密码0000 on 22/08/25.
+//
+
+import SwiftUI
+
+@main
+struct PixelPlayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
